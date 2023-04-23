25a2a6887d566c67d5ab5b43962e28daf26a80e4
margo.bezmosciuc <margo.bezmosciuc@gmail.com>
2023-04-23 22:43:04 +0200

    working on contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import "./contact.scss"
+import shake from "../../assets/shake.svg"
 
 export default function Contact() {
   return (
     <div className='contact' id='contact'>
-      
+      <div className="left">
+        <img src={shake} alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form action="">
+          <input type="text" placeholder='Email' />
+          <textarea placeholder='Write me here...'></textarea>
+          <button type='submit'>Send</button>
+        </form>
+      </div>
     </div>
   )
 }
+
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,21 +1,60 @@
 import React from "react";
 import "./testimonials.scss";
-import rightArrow from "../../assets/right-arrow.png";
+import rightArrow from "../../assets/right-arrow3.png";
+//import youtube from "../../assets/youtube.png";
+//import twitter from "../../assets/twitter.png";
+import linkedin from "../../assets/linkedin.png";
+import slack from "../../assets/slack.png"
+import xing from "../../assets/xing.png"
 
 export default function Testimonials() {
+  const data = [
+    {
+      id: 1,
+      name: "Nargiza Rysmendieva",
+      title: "Teacher Assistant DCI",
+      img: "https://media.licdn.com/dms/image/C4E03AQExau4gbeAqYQ/profile-displayphoto-shrink_200_200/0/1616576815389?e=1687996800&v=beta&t=n7jeHzEY5vgYzddbEWK9H1vMQF8TvoKaSx1CtW215BA",
+      icon: slack,
+      desc: "Make me Crazy with your colors",
+    },
+    {
+      id: 2,
+      name: "Dilshod Rahmanov",
+      title: "Full Stack Web Development Lecturer at DCI",
+      img: "https://media.licdn.com/dms/image/C5603AQEt_g23zdxqeQ/profile-displayphoto-shrink_200_200/0/1519491994255?e=1687996800&v=beta&t=58sriH7q8QYyQnBApJJ5yZGvVAWhrQ5LWEfhAB4sZQE",
+      icon:  linkedin,
+      desc: "Dilshod didn't knew that have to say something: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit assumenda a architecto provident, veritatis eaque vero modi laudantium nemo culpa aut, eligendi  ",
+      featured: true,
+    },
+    {
+      id: 3,
+      name: "Rammiz Mammadov",
+      title: "Team Leader Fly Line",
+      img: "https://media.licdn.com/dms/image/C4E03AQE2HFd6PWmMdQ/profile-displayphoto-shrink_200_200/0/1659386189334?e=1687996800&v=beta&t=E-fecQua2uOKnnwwkiRj85eX5bogDTcCxKQ3y1pI16A",
+      icon: xing,
+      desc: "MarGo is one who values and respects colleges, creating a positive and supportive work environment",
+    },
+  ];
   return (
     <div className="testimonials" id="testimonials">
       <h1>Testimonials</h1>
       <div className="container">
-        <div className="card">
-          <div className="top">
-            <img src={rightArrow} className="left" alt="" />
-            <img src="https://images.unsplash.com/photo-1507208773393-40d9fc670acf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
-            <img src="" alt="" />
-          </div>
-          <div className="center"></div>
-          <div className="button"></div>
-        </div>
+        {data.map((d) => {
+          return (
+            <div className={d.featured ? "card featured" : "card"}>
+              <div className="top">
+                <img src={rightArrow} className="left" alt="" />
+                <img className="user" src={d.img} alt="" />
+                <img className="right" src={d.icon} alt="" />
+              </div>
+              <div className="center">{d.desc}</div>
+              <div className="bottom">
+                <h3>{d.name} </h3>
+                <h4>{d.title} </h4>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
